e9651cb233c6691c328e6ab1eac074bf1a604f0c
Debbie Cheng <dqrcheng@gmail.com>
2021-03-05 23:14:07 +1100

    Adjust margin

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,11 @@ import './App.css';
 import Game from './components/Game';
 
 function App() {
-  
+  const lang: string = window.navigator.language;
+
   return (
     <div className="App">
-      <h1 className="title">Boop a Cat!</h1>
+      {lang === "zh-TW" ? <h1 className="title">來和小貓玩</h1>:<h1 className="title">Boop a Cat!</h1> }
       <Game />
     </div>
   );
