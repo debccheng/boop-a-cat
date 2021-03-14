18e8de5a58fd09ee24d073d1ce40f9d6b84b8430
Debbie Cheng <dqrcheng@gmail.com>
2021-03-14 12:41:46 +1100

    Added footer

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,9 @@ function App() {
     <div className="App">
       {lang === "zh-TW" ? <h1 className="title">來和小貓玩</h1>:<h1 className="title">Boop a Cat!</h1> }
       <Game />
+      <footer className="footer">
+        by&nbsp;<a href="https://debccheng.com" target="_blank" rel="noreferrer">Deb</a>, with love.
+      </footer>
     </div>
   );
 }
